8c4b280cab3d80a574b378d2c176b7971a9c755a
David Vivanco <72324297+DavSeb21031990@users.noreply.github.com>
2025-07-28 03:41:58 +0000

    Cambiar estructura

--- Tareas/Semana_1/Thursday/Exercise_2.py
+++
@@ -1,8 +0,0 @@
-import random
-
-lat = random.uniform(-90.0, 90.0)
-lon = random.uniform(-180.0, 180.0)
-
-coords = (lat, lon)
-coords[0] = 0
-print(f"Latitude: {coords[0]}, Longitude: {coords[1]}")
