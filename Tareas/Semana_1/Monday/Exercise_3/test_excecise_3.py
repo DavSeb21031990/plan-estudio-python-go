7886ec2e0e9d729928d94487a4150d01261c54a9
David Vivanco <72324297+DavSeb21031990@users.noreply.github.com>
2025-07-28 04:20:03 +0000

    Cambiar estructura

--- Tareas/Semana_1/Monday/Exercise_3/test_excecise_3.py
+++
@@ -1,8 +0,0 @@
-from main import calculateArea
-
-
-def test_calculateArea(capsys, monkeypatch):
-    monkeypatch.setattr("builtins.input", lambda _: "5")
-    calculateArea()
-    response = capsys.readouterr()
-    assert response.out == "Resultado: El área del circulo es 78.54\n"
--- Tareas/Semana_1/Thursday/Excercise_3.py
+++
@@ -1,2 +0,0 @@
-result = [num for num in range(1, 11) if num % 2 == 0]
-print(result)
